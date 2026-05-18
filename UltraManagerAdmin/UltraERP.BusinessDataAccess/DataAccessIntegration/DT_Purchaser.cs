ec264b2f3b27f62d6fcd3b3009ee8dbde4885b04
Roilan Cubillo Meza <126511039+RoilanCubilloMeza@users.noreply.github.com>
2026-05-18 07:38:01 -0700

    casa comerciales listo

--- UltraManagerAdmin/UltraERP.BusinessDataAccess/DataAccessIntegration/DT_Purchaser.cs
+++ UltraManagerAdmin/UltraERP.BusinessDataAccess/DataAccessIntegration/DT_Purchaser.cs
@@ -1,9 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using UltraERP.BusinessDataAccess.Data;
 using UltraERP.BusinessEntities;
 
@@ -11,43 +10,237 @@ namespace UltraERP.BusinessDataAccess.DataAccessIntegration
 {
     public class DT_Purchaser : DT
     {
-        #region Variables
-        #endregion
-
-        #region Constructor
         public DT_Purchaser() : base() { }
-        #endregion
 
-        #region Métodos
         public List<EN_Purchaser> GetAll()
         {
-            List<EN_Purchaser> list =
-                (from p in db.UEP_PURCHASER_GETALL()
-                 select new EN_Purchaser() { ID = p.ID, Code = p.Code, Name = p.Name, Inactive = p.Inactive, EmailAddress = p.EmailAddress, ExtCode = p.ExtCode, Telephone = p.Telephone, LastUpdated = p.LastUpdated }
-                ).ToList();
-            
-            return list;
+            try
+            {
+                return (from p in db.UEP_PURCHASER_GETALL()
+                        select new EN_Purchaser
+                        {
+                            ID = p.ID,
+                            Code = p.Code,
+                            Name = p.Name,
+                            Inactive = p.Inactive,
+                            EmailAddress = p.EmailAddress,
+                            ExtCode = p.ExtCode,
+                            Telephone = p.Telephone,
+                            LastUpdated = p.LastUpdated
+                        }).ToList();
+            }
+            catch (SqlException)
+            {
+                return GetAllDirect(null);
+            }
         }
 
         public List<EN_Purchaser> GetAllByInactive(bool inactive)
         {
-            List<EN_Purchaser> list =
-                (from p in db.UEP_PURCHASER_GETALL_BY_INACTIVE(inactive)
-                 select new EN_Purchaser() { ID = p.ID, Code = p.Code, Name = p.Name, Inactive = p.Inactive, EmailAddress = p.EmailAddress, ExtCode = p.ExtCode, Telephone = p.Telephone, LastUpdated = p.LastUpdated }
-                ).ToList();
-
-            return list;
+            try
+            {
+                return (from p in db.UEP_PURCHASER_GETALL_BY_INACTIVE(inactive)
+                        select new EN_Purchaser
+                        {
+                            ID = p.ID,
+                            Code = p.Code,
+                            Name = p.Name,
+                            Inactive = p.Inactive,
+                            EmailAddress = p.EmailAddress,
+                            ExtCode = p.ExtCode,
+                            Telephone = p.Telephone,
+                            LastUpdated = p.LastUpdated
+                        }).ToList();
+            }
+            catch (SqlException)
+            {
+                return GetAllDirect(inactive);
+            }
         }
 
         public Respuesta Save(EN_Purchaser pur)
         {
-            Respuesta respuesta = new Respuesta("NO ACCIONADO", "error_guardar", null, false);
-            
-            foreach(var i in db.UEP_PURCHASER_INSERT_UPDATE(pur.ID, pur.Code, pur.ExtCode, pur.Name, pur.EmailAddress, pur.Telephone, pur.Inactive))
-                respuesta = new Respuesta("", i.RESPUESTA, i.SCOPE, !i.RESPUESTA.Contains("error"));
+            if (pur == null)
+                return new Respuesta("PURCHASER_NULL", "No se recibio la casa comercial.", null, false);
 
-            return respuesta;
+            try
+            {
+                if (ProcedureExists("UEP_PURCHASER_INSERT_UPDATE"))
+                {
+                    Respuesta respuesta = new Respuesta("NO ACCIONADO", "error_guardar", null, false);
+
+                    foreach (var i in db.UEP_PURCHASER_INSERT_UPDATE(
+                        pur.ID,
+                        pur.Code,
+                        pur.ExtCode,
+                        pur.Name,
+                        pur.EmailAddress,
+                        pur.Telephone,
+                        pur.Inactive))
+                    {
+                        respuesta = new Respuesta("", i.RESPUESTA, i.SCOPE, !i.RESPUESTA.Contains("error"));
+                    }
+
+                    return respuesta;
+                }
+            }
+            catch (SqlException ex)
+            {
+                if (!IsMissingProcedure(ex))
+                    throw;
+            }
+
+            return SaveDirect(pur);
         }
-        #endregion
+
+        private List<EN_Purchaser> GetAllDirect(bool? inactive)
+        {
+            var purchasers = new List<EN_Purchaser>();
+
+            using (var connection = new SqlConnection(con.CadenaConexion))
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandType = CommandType.Text;
+                command.CommandText = @"
+                    SELECT ID, Code, ExtCode, Name, EmailAddress, Telephone, Inactive, LastUpdated
+                    FROM dbo.POA_Purchaser
+                    WHERE (@Inactive IS NULL OR Inactive = @Inactive)
+                    ORDER BY Name, Code";
+
+                command.Parameters.AddWithValue("@Inactive", inactive.HasValue ? (object)inactive.Value : DBNull.Value);
+                connection.Open();
+
+                using (SqlDataReader dataReader = command.ExecuteReader())
+                {
+                    while (dataReader.Read())
+                        purchasers.Add(MakePurchaser(dataReader));
+                }
+            }
+
+            return purchasers;
+        }
+
+        private Respuesta SaveDirect(EN_Purchaser pur)
+        {
+            using (var connection = new SqlConnection(con.CadenaConexion))
+            using (var command = connection.CreateCommand())
+            {
+                connection.Open();
+                command.CommandType = CommandType.Text;
+
+                bool exists = false;
+
+                if (pur.ID > 0)
+                {
+                    command.CommandText = "SELECT COUNT(1) FROM dbo.POA_Purchaser WHERE ID = @ID";
+                    command.Parameters.AddWithValue("@ID", pur.ID);
+                    exists = Convert.ToInt32(command.ExecuteScalar()) > 0;
+                    command.Parameters.Clear();
+                }
+
+                command.Parameters.AddWithValue("@Code", SafeValue(pur.Code, 20));
+                command.Parameters.AddWithValue("@ExtCode", SafeValue(pur.ExtCode, 20));
+                command.Parameters.AddWithValue("@Name", SafeValue(pur.Name, 50));
+                command.Parameters.AddWithValue("@EmailAddress", SafeValue(pur.EmailAddress, 255));
+                command.Parameters.AddWithValue("@Telephone", SafeValue(pur.Telephone, 30));
+                command.Parameters.AddWithValue("@Inactive", pur.Inactive);
+
+                if (exists)
+                {
+                    command.CommandText = @"
+                        UPDATE dbo.POA_Purchaser
+                        SET Code = @Code,
+                            ExtCode = @ExtCode,
+                            Name = @Name,
+                            EmailAddress = @EmailAddress,
+                            Telephone = @Telephone,
+                            Inactive = @Inactive,
+                            LastUpdated = GETDATE()
+                        WHERE ID = @ID;
+
+                        SELECT @ID;";
+
+                    command.Parameters.AddWithValue("@ID", pur.ID);
+                }
+                else
+                {
+                    command.CommandText = @"
+                        INSERT INTO dbo.POA_Purchaser
+                        (
+                            Code,
+                            ExtCode,
+                            Name,
+                            EmailAddress,
+                            Telephone,
+                            Inactive,
+                            LastUpdated,
+                            HQId,
+                            SyncGuid
+                        )
+                        VALUES
+                        (
+                            @Code,
+                            @ExtCode,
+                            @Name,
+                            @EmailAddress,
+                            @Telephone,
+                            @Inactive,
+                            GETDATE(),
+                            NULL,
+                            NEWID()
+                        );
+
+                        SELECT CAST(SCOPE_IDENTITY() AS INT);";
+                }
+
+                object scope = command.ExecuteScalar();
+                return new Respuesta("", "Casa comercial guardada correctamente.", scope, true);
+            }
+        }
+
+        private bool ProcedureExists(string procedureName)
+        {
+            using (var connection = new SqlConnection(con.CadenaConexion))
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandType = CommandType.Text;
+                command.CommandText = @"
+                    SELECT COUNT(1)
+                    FROM sys.objects
+                    WHERE type = 'P' AND name = @ProcedureName";
+
+                command.Parameters.AddWithValue("@ProcedureName", procedureName);
+                connection.Open();
+                return Convert.ToInt32(command.ExecuteScalar()) > 0;
+            }
+        }
+
+        private static bool IsMissingProcedure(SqlException ex)
+        {
+            return ex != null &&
+                   (ex.Number == 2812 ||
+                    ex.Message.IndexOf("Could not find stored procedure", StringComparison.OrdinalIgnoreCase) >= 0);
+        }
+
+        private static string SafeValue(string value, int maxLength)
+        {
+            string cleanValue = (value ?? String.Empty).Trim();
+            return cleanValue.Length > maxLength ? cleanValue.Substring(0, maxLength) : cleanValue;
+        }
+
+        private static EN_Purchaser MakePurchaser(SqlDataReader dataReader)
+        {
+            return new EN_Purchaser
+            {
+                ID = dataReader.IsDBNull(dataReader.GetOrdinal("ID")) ? 0 : dataReader.GetInt32(dataReader.GetOrdinal("ID")),
+                Code = dataReader.IsDBNull(dataReader.GetOrdinal("Code")) ? String.Empty : dataReader.GetString(dataReader.GetOrdinal("Code")),
+                ExtCode = dataReader.IsDBNull(dataReader.GetOrdinal("ExtCode")) ? String.Empty : dataReader.GetString(dataReader.GetOrdinal("ExtCode")),
+                Name = dataReader.IsDBNull(dataReader.GetOrdinal("Name")) ? String.Empty : dataReader.GetString(dataReader.GetOrdinal("Name")),
+                EmailAddress = dataReader.IsDBNull(dataReader.GetOrdinal("EmailAddress")) ? String.Empty : dataReader.GetString(dataReader.GetOrdinal("EmailAddress")),
+                Telephone = dataReader.IsDBNull(dataReader.GetOrdinal("Telephone")) ? String.Empty : dataReader.GetString(dataReader.GetOrdinal("Telephone")),
+                Inactive = !dataReader.IsDBNull(dataReader.GetOrdinal("Inactive")) && dataReader.GetBoolean(dataReader.GetOrdinal("Inactive")),
+                LastUpdated = dataReader.IsDBNull(dataReader.GetOrdinal("LastUpdated")) ? DateTime.MinValue : dataReader.GetDateTime(dataReader.GetOrdinal("LastUpdated"))
+            };
+        }
     }
 }
--- UltraManagerAdmin/UltraERP/Controllers/CasasComercialesController.cs
+++ UltraManagerAdmin/UltraERP/Controllers/CasasComercialesController.cs
@@ -1,7 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web.Mvc;
+using UltraERP.BusinessEntities;
+using UltraERP.BusinessLogic;
 using UltraERP.Models;
 
 namespace UltraERP.Controllers
@@ -9,37 +14,43 @@ namespace UltraERP.Controllers
     [Authorize]
     public class CasasComercialesController : Controller
     {
-        private static readonly object SyncRoot = new object();
-
-        private static readonly List<CasaComercialViewModel> CasasComerciales = new List<CasaComercialViewModel>
-        {
-            CreateCasa(1, "CC-DP", "DOSPINOS", "Casa Comercial Dos Pinos", "compras@dospinos.co.cr", "2247-8000", false, 18),
-            CreateCasa(2, "CC-LIZ", "LIZANO", "Casa Comercial Lizano", "abasto@lizano.cr", "2220-4100", false, 9),
-            CreateCasa(3, "CC-BRT", "BRITT", "Casa Comercial Cafe Britt", "retail@brittcostarica.com", "2277-1600", false, 7),
-            CreateCasa(4, "CC-IRX", "IREX", "Casa Comercial Irex", "ventas@irex.cr", "2296-2200", false, 12),
-            CreateCasa(5, "CC-FLB", "FLORIDA", "Casa Comercial Florida Bebidas", "abastecimiento@florida.co.cr", "2437-7000", false, 11),
-            CreateCasa(6, "CC-NMR", "NUMAR", "Casa Comercial Numar", "servicioalcliente@numar.net", "2209-9800", true, 4)
-        };
-
         public ActionResult Inicio()
         {
-            List<CasaComercialViewModel> model;
-            lock (SyncRoot)
+            try
             {
-                model = CasasComerciales.Select(Clone).OrderBy(x => x.Nombre).ToList();
-            }
+                IDictionary<int, int> articulosAsociados = GetArticulosAsociadosPorCasa();
+                List<CasaComercialViewModel> model = new CT_Purchaser()
+                    .GetAll()
+                    .Select(x => MapCasa(x, articulosAsociados))
+                    .OrderBy(x => x.Nombre)
+                    .ToList();
 
-            return View(model);
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                TempData["CasaComercialError"] = "No se pudieron cargar las casas comerciales desde SQL: " + ex.Message;
+                return View(Enumerable.Empty<CasaComercialViewModel>());
+            }
         }
 
         public ActionResult Registro(int? id)
         {
             CasaComercialViewModel model = null;
+
             if (id.HasValue)
             {
-                lock (SyncRoot)
+                try
                 {
-                    model = CasasComerciales.Where(x => x.ID == id.Value).Select(Clone).FirstOrDefault();
+                    EN_Purchaser purchaser = GetCasaById(id.Value);
+                    if (purchaser == null)
+                        TempData["CasaComercialError"] = "No se encontro la casa comercial en SQL.";
+                    else
+                        model = MapCasa(purchaser, GetArticulosAsociadosPorCasa());
+                }
+                catch (Exception ex)
+                {
+                    TempData["CasaComercialError"] = "No se pudo leer la casa comercial desde SQL: " + ex.Message;
                 }
             }
 
@@ -56,29 +67,44 @@ namespace UltraERP.Controllers
             if (!ModelState.IsValid)
                 return View(model);
 
-            lock (SyncRoot)
+            try
             {
-                var existing = CasasComerciales.FirstOrDefault(x => x.ID == model.ID);
-                if (existing == null)
+                EN_Purchaser purchaser = new EN_Purchaser
                 {
-                    model.ID = CasasComerciales.Count == 0 ? 1 : CasasComerciales.Max(x => x.ID) + 1;
-                    model.UsuarioCrea = GetCurrentUser();
-                    model.FechaCrea = DateTime.Now;
-                    CasasComerciales.Add(Clone(model));
-                    TempData["CasaComercialMessage"] = "Casa comercial creada correctamente.";
-                }
-                else
-                {
-                    model.UsuarioCrea = existing.UsuarioCrea;
-                    model.FechaCrea = existing.FechaCrea;
-                    model.UsuarioModifica = GetCurrentUser();
-                    model.FechaModifica = DateTime.Now;
-                    CopyValues(model, existing);
-                    TempData["CasaComercialMessage"] = "Casa comercial actualizada correctamente.";
-                }
-            }
+                    ID = model.ID,
+                    Code = model.Codigo,
+                    ExtCode = model.CodigoExtra ?? "",
+                    Name = model.Nombre,
+                    EmailAddress = model.CorreoElectronico,
+                    Telephone = model.Telefono,
+                    Inactive = model.Inactivo
+                };
 
-            return RedirectToAction("Inicio");
+                Respuesta response = new CT_Purchaser().Save(purchaser);
+                if (!response.Status)
+                {
+                    ModelState.AddModelError("", "SQL rechazo el guardado de la casa comercial: " + response.Message);
+                    return View(model);
+                }
+
+                TempData["CasaComercialMessage"] = model.ID > 0
+                    ? "Casa comercial actualizada correctamente en SQL."
+                    : "Casa comercial creada correctamente en SQL.";
+
+                return RedirectToAction("Inicio");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "No se pudo guardar la casa comercial en SQL. " + ex.Message);
+                return View(model);
+            }
+        }
+
+        private EN_Purchaser GetCasaById(int id)
+        {
+            return new CT_Purchaser()
+                .GetAll()
+                .FirstOrDefault(x => x.ID == id);
         }
 
         private void ValidateCasa(CasaComercialViewModel model)
@@ -86,20 +112,99 @@ namespace UltraERP.Controllers
             if (model == null)
                 return;
 
-            lock (SyncRoot)
+            try
             {
-                var duplicatedCode = CasasComerciales.Any(x =>
+                List<EN_Purchaser> purchasers = new CT_Purchaser().GetAll();
+
+                bool duplicatedCode = purchasers.Any(x =>
                     x.ID != model.ID &&
-                    String.Equals(x.Codigo, model.Codigo, StringComparison.OrdinalIgnoreCase));
+                    String.Equals(x.Code, model.Codigo, StringComparison.OrdinalIgnoreCase));
+
                 if (duplicatedCode)
                     ModelState.AddModelError("Codigo", "Ya existe una casa comercial con este codigo.");
 
-                var duplicatedName = CasasComerciales.Any(x =>
+                bool duplicatedName = purchasers.Any(x =>
                     x.ID != model.ID &&
-                    String.Equals(x.Nombre, model.Nombre, StringComparison.OrdinalIgnoreCase));
+                    String.Equals(x.Name, model.Nombre, StringComparison.OrdinalIgnoreCase));
+
                 if (duplicatedName)
                     ModelState.AddModelError("Nombre", "Ya existe una casa comercial con este nombre.");
             }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "No se pudo validar la casa comercial contra SQL. " + ex.Message);
+            }
+        }
+
+        private IDictionary<int, int> GetArticulosAsociadosPorCasa()
+        {
+            var result = new Dictionary<int, int>();
+            ConnectionStringSettings settings = ConfigurationManager.ConnectionStrings["UltraERP.BusinessDataAccess.Properties.Settings.MasterDB"];
+
+            if (settings == null || String.IsNullOrWhiteSpace(settings.ConnectionString))
+                return result;
+
+            try
+            {
+                using (var connection = new SqlConnection(settings.ConnectionString))
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandType = CommandType.Text;
+                    command.CommandText = @"
+                        SELECT PurchaserID, COUNT(1) AS ItemCount
+                        FROM dbo.ExtCentral_Item
+                        WHERE PurchaserID IS NOT NULL AND PurchaserID > 0
+                        GROUP BY PurchaserID";
+
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            int purchaserID = reader.IsDBNull(reader.GetOrdinal("PurchaserID"))
+                                ? 0
+                                : Convert.ToInt32(reader["PurchaserID"]);
+
+                            if (purchaserID <= 0)
+                                continue;
+
+                            result[purchaserID] = reader.IsDBNull(reader.GetOrdinal("ItemCount"))
+                                ? 0
+                                : Convert.ToInt32(reader["ItemCount"]);
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                return result;
+            }
+
+            return result;
+        }
+
+        private static CasaComercialViewModel MapCasa(EN_Purchaser purchaser, IDictionary<int, int> articulosAsociados)
+        {
+            if (purchaser == null)
+                return null;
+
+            DateTime fechaBase = purchaser.LastUpdated == DateTime.MinValue ? DateTime.Now : purchaser.LastUpdated;
+
+            return new CasaComercialViewModel
+            {
+                ID = purchaser.ID,
+                Codigo = purchaser.Code,
+                CodigoExtra = purchaser.ExtCode,
+                Nombre = purchaser.Name,
+                CorreoElectronico = purchaser.EmailAddress,
+                Telefono = purchaser.Telephone,
+                Inactivo = purchaser.Inactive,
+                ArticulosAsociados = articulosAsociados.ContainsKey(purchaser.ID) ? articulosAsociados[purchaser.ID] : 0,
+                UsuarioCrea = "SQL",
+                FechaCrea = fechaBase,
+                UsuarioModifica = "SQL",
+                FechaModifica = purchaser.LastUpdated == DateTime.MinValue ? (DateTime?)null : purchaser.LastUpdated
+            };
         }
 
         private static void Normalize(CasaComercialViewModel model)
@@ -119,65 +224,9 @@ namespace UltraERP.Controllers
         {
             return new CasaComercialViewModel
             {
-                FechaCrea = DateTime.Now,
-                UsuarioCrea = GetCurrentUser()
+                UsuarioCrea = GetCurrentUser(),
+                FechaCrea = DateTime.Now
             };
-        }
-
-        private static CasaComercialViewModel CreateCasa(int id, string codigo, string codigoExtra, string nombre, string correo, string telefono, bool inactivo, int articulosAsociados)
-        {
-            return new CasaComercialViewModel
-            {
-                ID = id,
-                Codigo = codigo,
-                CodigoExtra = codigoExtra,
-                Nombre = nombre,
-                CorreoElectronico = correo,
-                Telefono = telefono,
-                Inactivo = inactivo,
-                ArticulosAsociados = articulosAsociados,
-                UsuarioCrea = "Soporte",
-                FechaCrea = DateTime.Now.AddDays(-5),
-                UsuarioModifica = inactivo ? "Soporte" : "Compras",
-                FechaModifica = DateTime.Now.AddDays(-1)
-            };
-        }
-
-        private static CasaComercialViewModel Clone(CasaComercialViewModel source)
-        {
-            if (source == null)
-                return null;
-
-            return new CasaComercialViewModel
-            {
-                ID = source.ID,
-                Codigo = source.Codigo,
-                CodigoExtra = source.CodigoExtra,
-                Nombre = source.Nombre,
-                CorreoElectronico = source.CorreoElectronico,
-                Telefono = source.Telefono,
-                Inactivo = source.Inactivo,
-                ArticulosAsociados = source.ArticulosAsociados,
-                UsuarioCrea = source.UsuarioCrea,
-                FechaCrea = source.FechaCrea,
-                UsuarioModifica = source.UsuarioModifica,
-                FechaModifica = source.FechaModifica
-            };
-        }
-
-        private static void CopyValues(CasaComercialViewModel source, CasaComercialViewModel target)
-        {
-            target.Codigo = source.Codigo;
-            target.CodigoExtra = source.CodigoExtra;
-            target.Nombre = source.Nombre;
-            target.CorreoElectronico = source.CorreoElectronico;
-            target.Telefono = source.Telefono;
-            target.Inactivo = source.Inactivo;
-            target.ArticulosAsociados = source.ArticulosAsociados;
-            target.UsuarioCrea = source.UsuarioCrea;
-            target.FechaCrea = source.FechaCrea;
-            target.UsuarioModifica = source.UsuarioModifica;
-            target.FechaModifica = source.FechaModifica;
         }
 
         private string GetCurrentUser()
